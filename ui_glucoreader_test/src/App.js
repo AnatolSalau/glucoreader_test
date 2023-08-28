0db10feed3f1b97b79ef23b73f78c05526407a38
AnatolSalau <salovanatoly5@mail.ru>
2023-08-28 14:29:12 +0300

    Add React User interface

---
+++ ui_glucoreader_test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+      return (
+            <div className="App">
+                  <text>
+                        Index text
+                  </text>
+            </div>
+      );
+}
+
+export default App;
