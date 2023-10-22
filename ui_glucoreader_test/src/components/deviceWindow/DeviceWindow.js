1851e626c1d2c348d39d40e53e2225e21ad6a12b
AnatolSalau <salovanatoly5@mail.ru>
2023-10-22 13:29:37 +0300

    Sunday 22 10 2023

--- ui_glucoreader_test/src/components/deviceWindow/DeviceWindow.js
+++ ui_glucoreader_test/src/components/deviceWindow/DeviceWindow.js
@@ -4,38 +4,47 @@ import {FcHighPriority, FcOk} from "react-icons/fc";
 import CheckBox from "../deviceList/CheckBox";
 import {useState} from "react";
 import {useEffect} from "react";
+
 let count = 0;
+
 function DeviceWindow({activeDeviceName, deviceList}) {
       console.log(activeDeviceName);
       console.log(count++);
-      let data = null;
+      let [data, setData] = useState(null);
       const getData = () => {
-            let data = null;
             deviceList.map(
                   (device) => {
                         if (device.comPortDto.name === activeDeviceName) {
-                              data = device;
-                              return data
+                              setData(device);
                         }
                   }
             )
-            return data;
       };
-      data = getData();
+      getData();
       return (
             <div className={style.deviceWindow}>
-
-                        <Section text={"Соединение:"}>
-                              {(data)
+                  {
+                        data
+                              ? <>
+                                    <Section text={"Соединение успешно"}/>
+                                    <Section/>
+                              </>
+                              :<>
+                                    <Section text={"Соединение неуспешно"}/>
+                                    <Section/>
+                              </>
+                  }
+{/*                  <Section text={"Соединение:"}>
+                        {(data)
                               ? <FcOk/>
                               : <FcHighPriority/>}
-                        </Section>
-                        <Section text= {(data)
-                              ? `Версия протокола: мин. вер = ${data.protocolVersionDto.versionHi}, макс. вер = ${data.protocolVersionDto.versionLo}`
-                              : <FcHighPriority/>}
-                        />
-                        <Section text={"Версия прибора:"}/>
-                        <Section text={"Состояние прибора:"}/>
+                  </Section>
+                  <Section text={(data)
+                        ? `Версия протокола: мин. вер = ${data.protocolVersionDto.versionHi}, макс. вер = ${data.protocolVersionDto.versionLo}`
+                        : <FcHighPriority/>}
+                  />
+                  <Section text={"Версия прибора:"}/>
+                  <Section text={"Состояние прибора:"}/>*/}
             </div>
       )
 }
