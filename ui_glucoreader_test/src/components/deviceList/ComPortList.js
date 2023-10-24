bd5289b0d339201996a209241638b078f142b18c
AnatolSalau <salovanatoly5@mail.ru>
2023-10-24 15:56:38 +0300

    Reload UI

--- ui_glucoreader_test/src/components/deviceList/ComPortList.js
+++ ui_glucoreader_test/src/components/deviceList/ComPortList.js
@@ -8,18 +8,21 @@ function ComPortList({text, activeDeviceName, setActiveDeviceNameHandler, device
                   <div className={style.text}>
                         {text}
                   </div>
-                  <div>
-                              <button
+
+                              <div
                                     onClick={() => {setDeviceListHandler()}}
                                     style={{
                                           width: "30px",
                                           height: "30px",
-                                          fontSize: "30px"
+                                          fontSize: "30px",
+                                          backgroundColor: "lightgray",
+                                          boxShadow: "rgba(0, 0, 0, 0.25) 0px 2px 4px",
+                                          borderRadius: "4px"
                               }}
                               >
                                     <TbReload/>
-                              </button>
-                  </div>
+                              </div>
+
                   <CheckBoxList
                         activeDeviceName={activeDeviceName}
                         setActiveDeviceNameHandler={setActiveDeviceNameHandler}
